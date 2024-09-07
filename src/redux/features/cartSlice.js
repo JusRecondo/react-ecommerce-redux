b069279471bb58d57ec90f5435eb6ff176f48874
JusRecondo <justina.recondo@gmail.com>
2024-09-07 10:39:18 -0300

    Remove comments

--- src/redux/features/cartSlice.js
+++ src/redux/features/cartSlice.js
@@ -5,17 +5,14 @@ const initialState = {
   productsInCart: []
 }
 
-// "parece que se esta mutando directamente el state", pero redux toolkit se ocupa por detras con la libreria immer
 export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    // recibe un objeto product como payload
     addProductToCart: (state, action) => {
       state.productsInCart = [...state.productsInCart, action.payload]
       state.totalCount += 1
     },
-    // recibe un id como payload
     removeProductFromCart: (state, action) => {
       state.productsInCart = state.productsInCart.filter(product => product.id !== action.payload)
       state.totalCount -= 1
