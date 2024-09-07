a1165a0556ddedb3fb7e2aa78c68f02ea2903831
JusRecondo <justina.recondo@gmail.com>
2024-09-07 16:57:27 -0300

    Add function to get product categories

--- src/services/products.js
+++ src/services/products.js
@@ -1,6 +1,6 @@
 export const getProducts = async () => {
   try {
-    const response = await fetch('https://dummyjson.com/products')
+    const response = await fetch('https://dummyjson.com/products?limit=50')
     if (!response.ok) {
       throw new Error(`Request failed with status ${response.status}`)
     }
@@ -14,6 +14,19 @@ export const getProducts = async () => {
 export const getProductById = async (id) => {
   try {
     const response = await fetch(`https://dummyjson.com/products/${id}`)
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${response.status}`)
+    }
+    const data = await response.json()
+    return data
+  } catch (e) {
+    throw new Error(e.message)
+  }
+}
+
+export const getProductCategories = async () => {
+  try {
+    const response = await fetch('https://dummyjson.com/products/category-list')
     if (!response.ok) {
       throw new Error(`Request failed with status ${response.status}`)
     }
