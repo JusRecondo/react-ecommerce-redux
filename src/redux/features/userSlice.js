166fe87dddcbe7bedd991693c6e748719ab2c389
JusRecondo <justina.recondo@gmail.com>
2024-09-08 17:40:03 -0300

    Add setRefreshedToken in userSlice

--- src/redux/features/userSlice.js
+++ src/redux/features/userSlice.js
@@ -1,23 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = {
-}
+const initialState = {}
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
     setUser: (state, action) => {
-      const { firstName, lastName, email, image, token } = action.payload
+      const { firstName, lastName, email, image, token, refreshToken } = action.payload
       state.fullName = firstName + ' ' + lastName
       state.email = email
       state.image = image
       state.token = token
+      state.refreshToken = refreshToken
+      /* state = { ...action.payload } */
+    },
+    setUserProfileData: (state, action) => {
+      const { phone, address, birthDate } = action.payload
+      state.phone = phone
+      state.address = address
+      state.birthDate = birthDate
+      /* state = { ...action.payload } */
+    },
+    setRefreshedToken: (state, action) => {
+      const { token, refreshToken } = action.payload
+      state.token = token
+      state.refreshToken = refreshToken
     },
     unsetUser: () => ({})
   }
 })
 
-export const { setUser, unsetUser } = userSlice.actions
+export const { setUser, setUserProfileData, setRefreshedToken, unsetUser } = userSlice.actions
 
 export default userSlice.reducer
