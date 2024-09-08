517a2ad066b0b6bd3b53bd7107b28b874941356f
JusRecondo <justina.recondo@gmail.com>
2024-09-08 17:37:13 -0300

    Add functions to get user profile data and refresh token

--- src/services/user.js
+++ src/services/user.js
@@ -14,3 +14,42 @@ export const userAuth = async (userData) => {
     throw new Error(e.message)
   }
 }
+
+// Get current user data
+export const getCurrentUserData = async (token) => {
+  try {
+    const response = await fetch('https://dummyjson.com/auth/me', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${response.status}`)
+    }
+    return await response.json()
+  } catch (e) {
+    throw new Error(e.message)
+  }
+}
+
+// Refresh user token
+export const refreshUserToken = async (refreshToken) => {
+  try {
+    const response = await fetch('https://dummyjson.com/auth/refresh', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        refreshToken
+      })
+    })
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${response.status}`)
+    }
+    return await response.json()
+  } catch (e) {
+    throw new Error((e).message)
+  }
+}
